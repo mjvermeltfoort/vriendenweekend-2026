3be1362eaab4f84c667f35210b584d3d6fa86adf
mjvermeltfoort <mjvermeltfoort@gmail.com>
2026-07-21 18:53:33 +0000

    Update appsScriptUrl to the correct execution link

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.VRIENDENWEEKEND_CONFIG = {
   // Vervang onderstaande URL door jouw gepubliceerde Apps Script /exec-link.
-  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbyu0wV-AJJB8vme9kDIVST_EESnEy577Ipz3teRO987_OYqyjKEM25DNDHT81h0IIyk4g/exec'
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbwFDWrZGvR-tOBZmlJO2t9qHKoR98mNC7BqNHPgfY1qIRemBtD2h1ealmuIX9euEyY1kA/exec'
 };
